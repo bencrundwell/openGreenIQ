e0c4a1f63417fa1e00aa7266f850a4cb5a0e92b9
Ben Crundwell <Ben@Crundwell.com>
2019-05-24 20:26:30 +0100

    add Vuex store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,15 +5,19 @@ import 'core-js/es6/string'
 import 'core-js/es7/array'
 // import cssVars from 'css-vars-ponyfill'
 import Vue from 'vue'
+import Vuex from 'vuex'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import store from "./stores/store";
 
 // import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
-// Vue.config.productionTip = false
+
+
+Vue.config.productionTip = false
 
 // new Vue({
 //   render: h => h(App),
@@ -22,6 +26,9 @@ Vue.use(BootstrapVue)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // provide the store using the "store" option.
+  // this will inject the store instance to all child components.
+  store,
   router,
   template: '<App/>',
   components: {
